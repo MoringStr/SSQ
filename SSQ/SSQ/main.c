6f74844201658c61c6c59da546700943c95ed2d4
MoringStr <MoringStr010@gmail.com>
2019-07-03 07:56:32 +0800

    add

---
+++ SSQ/SSQ/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SSQ
+//
+//  Created by MoringStar on 2019/7/3.
+//  Copyright © 2019 MoringStar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
